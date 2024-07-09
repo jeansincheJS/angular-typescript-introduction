936217f81506d61914a28e27f97f257d3ee7744b
JeanS <jean.sinche@alarcongroup.us>
2024-07-08 20:04:14 -0500

    02 object interface

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
-import './topics/01-basic-types.ts'
+// import './topics/01-basic-types.ts'
+import './topics/02-object-interface.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
