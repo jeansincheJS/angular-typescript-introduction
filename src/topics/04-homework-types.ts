3b7bc5c99b27495d55005fed53ba287022f70c3f
JeanS <jean.sinche@alarcongroup.us>
2024-07-09 23:50:35 -0500

    homework

--- src/topics/04-homework-types.ts
+++ src/topics/04-homework-types.ts
@@ -9,20 +9,20 @@ interface SuperHero {
     showAddress: () => string;
 }
 interface Address {
-    calle: string;
-    pais: string;
-    ciudad: string;
+    street: string;
+    country: string;
+    city: string;
 }
 const superHeroe: SuperHero = {
     name: 'Spiderman',
     age: 30,
     address: {
-        calle: 'Main St',
-        pais: 'USA',
-        ciudad: 'NY'
+        street: 'Main St',
+        country: 'USA',
+        city: 'NY'
     },
     showAddress() {
-        return this.name + ', ' + this.address.ciudad + ', ' + this.address.pais;
+        return this.name + ', ' + this.address.city + ', ' + this.address.country;
     }
 }
 
